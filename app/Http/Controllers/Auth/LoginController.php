8462c37af5603e2a9e6fbb550d72c7d8a48397d7
unknown <bobby.gerez@yahoo.com>
2020-02-24 12:55:10 +0800

    Added Passport

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
+use Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -17,7 +20,7 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-    */
+     */
 
     use AuthenticatesUsers;
 
@@ -36,5 +39,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            return $this->success('email', $request->email);
+        } else {
+            return response()->json(['error' => 'Invalid Username or Password.'], 401);
+        }
+    }
+
+    public function success($field, $value)
+    {
+        $request = app()->make('request');
+        $user = User::where($field, $value)->first();
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        return response()->json([
+            'success' => $success,
+            'user' => $user,
+        ]);
     }
 }
--- resources/js/pages/userHome.vue
+++ resources/js/pages/userHome.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
-        Home
+        <input type="texbox" v-model="email" /> <br />
+        <input type="password" v-model="password" />
+        <button @click="submit">Submit</button>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            email: "",
+            password: ""
+        };
+    },
+    methods: {
+        submit() {
+            axios.post("login", {
+                email: this.email,
+                password: this.password
+            });
+        }
+    }
+};
+</script>
